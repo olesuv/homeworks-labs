e5991a4e2aafb7e37261b31ea5f0c23ba8b03669
plxgwalker <olegsuv.ukr@gmail.com>
2023-04-16 10:49:32 +0300

    Test variant of scattering fields.

--- applied-statistics-and-probabilistic-processes/lab-5/main.py
+++ applied-statistics-and-probabilistic-processes/lab-5/main.py
@@ -35,10 +35,27 @@ def generate_random_list_x(a: int, b: int, sigma: int, list_amount: int) -> list
     return sample
 
 
+def generate_random_list_x_y(a: int, b: int, sigma: int, list_amount: int) -> list:
+    sample = []
+
+    while len(sample) < list_amount:
+        x = randint(a, b)
+        y = randint(a, b)
+
+        p_x = exp(-((x - (a+b)/2) / sigma)**2/2) / (sigma * sqrt(2*pi))
+        p_y = exp(-((y - (a+b)/2) / sigma)**2/2) / (sigma * sqrt(2*pi))
+
+        if random() < p_x and random() < p_y:
+            sample.append([x, y])
+
+    return sample
+
+
 def plot_variancial(sequence: list) -> None:
     counts = Counter(sequence)
     x = list(counts.keys())
     y = cumsum(list(counts.values())) / len(sequence)
+
     plt.plot(x, y, drawstyle="steps")
     plt.xticks(x)
     plt.title("Діаграма накопичених частот")
@@ -50,6 +67,7 @@ def plot_variancial(sequence: list) -> None:
 
 def plot_histogram(sequence: list, bins=20) -> None:
     bins = list(range(int(min(sequence)), int(max(sequence))+2))
+
     plt.hist(sequence, bins=bins, width=1, edgecolor="black")
     plt.xticks(range(int(min(sequence)), int(max(sequence))+1))
     plt.title("Діаграма накопичених частот")
@@ -58,21 +76,37 @@ def plot_histogram(sequence: list, bins=20) -> None:
     plt.show()
 
 
+def plot_scattering_fields(sequence: list) -> None:
+    x = [d[0] for d in sequence]
+    y = [d[1] for d in sequence]
+
+    plt.scatter(x, y)
+    plt.title("Поля розсіяння")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.grid(True)
+    plt.show()
+
+
 if __name__ == "__main__":
     A = 0
     B = 20
-    N = 30
+    N = 5
     STD = 15
 
-    result = generate_random_list_x(A, B, STD, N)
+    # result = generate_random_list_x(A, B, STD, N)
     # result = [2, 4, 5, 5, 7]
 
-    print(
-        f"{result}\n"
-        f"Математичне сподівання: {custom_mean(result, N):.2f}, "
-        f"Дисперсія: {custom_variance(result, N):.2f}, "
-        f"Середньоквадратичне відхилення: {custom_devidation(result, N):.2f}"
-    )
+    # print(
+    #     f"{result}\n"
+    #     f"Математичне сподівання: {custom_mean(result, N):.2f}, "
+    #     f"Дисперсія: {custom_variance(result, N):.2f}, "
+    #     f"Середньоквадратичне відхилення: {custom_devidation(result, N):.2f}"
+    # )
 
-    plot_variancial(result)
-    plot_histogram(result)
+    # plot_variancial(result)
+    # plot_histogram(result)
+
+    result = generate_random_list_x_y(A, B, STD, N)
+    print(result)
+    plot_scattering_fields(result)
