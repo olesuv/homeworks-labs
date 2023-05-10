df7a8ac00356ea8d45e1ca11b258b95a08e6e66d
plxgwalker <olegsuv.ukr@gmail.com>
2023-05-10 11:43:16 +0300

    Closed lab-8.

--- parallel-programming/lab-8/lab-8.c
+++ parallel-programming/lab-8/lab-8.c
@@ -1,53 +1,57 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
 
-#define MATRIX_SIZE 3
+#define ROWS 3
+#define COLS 3
 
 int main(int argc, char** argv) {
     int rank, size;
-    int matrix[MATRIX_SIZE][MATRIX_SIZE];
-    int row_max[MATRIX_SIZE], row_sum = 0;
-    double time_start = 0, time_finish = 0;
+    int matrix[ROWS][COLS];
+    int local_max[ROWS];
+    int global_max[ROWS];
+    int local_sum = 0;
+    int global_sum = 0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Fill the matrix with random values
-    for (int i = 0; i < MATRIX_SIZE; i++) {
-        for (int j = 0; j < MATRIX_SIZE; j++) {
+    // Initialize the matrix with random values
+    srand(rank);
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
             matrix[i][j] = rand() % 100;
         }
     }
 
-    // Find the maximum value in each row
-    for (int i = 0; i < MATRIX_SIZE; i++) {
-        int max = 0;
-        for (int j = 0; j < MATRIX_SIZE; j++) {
-            if (matrix[i][j] > max) {
-                max = matrix[i][j];
+    // Find the local maximum element in each row
+    for (int i = 0; i < ROWS; i++) {
+        local_max[i] = matrix[i][0];
+        for (int j = 1; j < COLS; j++) {
+            if (matrix[i][j] > local_max[i]) {
+                local_max[i] = matrix[i][j];
             }
         }
-        row_max[i] = max;
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    // Compute the local sum of the maximum elements
+    for (int i = 0; i < ROWS; i++) {
+        local_sum += local_max[i];
+    }
 
-    time_start = MPI_Wtime();
-    MPI_Reduce(row_max, &row_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    time_finish = MPI_Wtime() - time_start;
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        for (int i = 0; i < MATRIX_SIZE; i++) {
-            for (int j = 0; j < MATRIX_SIZE; j++) {
+        printf("Matrix:\n");
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
                 printf("%d ", matrix[i][j]);
             }
             printf("\n");
         }
 
-        printf("Sum of maximum values: %d\n", row_sum);
-        printf("Reduce time: %f\n", time_finish);
+        printf("Sum of the maximum elements in the rows of the matrix with %d amount of processes: %d\n", size, global_sum);
     }
 
     MPI_Finalize();
