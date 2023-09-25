0bde049986ffa30e28b268a8033dc99498dd96b3
plxgwalker <olegsuv.ukr@gmail.com>
2023-09-25 21:03:15 +0300

    add: lab-2 without aproximation

---
+++ fuzzy-systems/lab-2/main.py
@@ -0,0 +1,40 @@
+from functools import reduce
+import matplotlib.pyplot as plt
+from fractions import Fraction
+
+
+def process_file(file_path):
+    results = []
+
+    with open(file_path, "r") as f:
+        data = [list(map(parse_value, line.split())) for line in f]
+
+    for row in data:
+        result = reduce(lambda x, y: float(x) * float(y),
+                        row[1:]) ** (1/7) / max(row[1:])
+        results.append(result)
+        print(result)
+
+    x_values = [row[0] for row in data]
+
+    y_values = results
+
+    plt.plot(x_values, y_values)
+    plt.title(f"Графік для файлу '{file_path}'")
+    plt.xlabel("Ріст")
+    plt.ylabel("Ступінь приналежності і-того елемента до нечіткої множини")
+    plt.show()
+
+
+def parse_value(value):
+    try:
+        return float(value)
+    except ValueError:
+        return float(Fraction(value))
+
+
+if __name__ == "__main__":
+    print("Tall man results:")
+    process_file("tall_man.txt")
+    print("\nShort man results:")
+    process_file("short_man.txt")
