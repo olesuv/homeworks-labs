dc47e3fe62997e83f07b70e97daa87f922f079f0
plxgwalker <olegsuv.ukr@gmail.com>
2023-10-10 17:00:49 +0300

    modify: dk if plots are ok

--- calculation-methods/lab-2/main.py
+++ calculation-methods/lab-2/main.py
@@ -6,29 +6,26 @@ def f(x):
     return math.sin(x)
 
 
-def generate_input_file():
+def write_input_data():
     a = 0
     b = 1
-    n = 20
+    n = 30
     h = (b - a) / n
-
-    with open("in.txt", "wt") as file1:
+    with open("in.txt", "w") as file1:
         for i in range(n+1):
-            x = a + i*h
+            x = a + i * h
             y = f(x)
-            file1.write(f"{x:e} {y:e}\n")
+            file1.write(f"{x} {y}\n")
 
 
-def read_data():
-    X = []
-    Y = []
-
-    with open("in.txt", "rt") as file:
-        for line in file:
+def readdata():
+    with open("in.txt", "r") as file:
+        lines = file.readlines()
+        X, Y = [], []
+        for line in lines:
             x, y = map(float, line.split())
             X.append(x)
             Y.append(y)
-
     return X, Y
 
 
@@ -57,37 +54,41 @@ def Nn(x, N, X, Y):
     return S
 
 
-def plot_file(filename, title, xlabel, ylabel):
+def plot_file(filename, title):
     with open(filename, 'r') as file:
-        data = [list(map(float, line.split())) for line in file]
-        x, y = zip(*data)
-
-    plt.figure()
-    plt.plot(x, y)
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
+        lines = file.readlines()
+        x_vals, y_vals = [], []
+        for line in lines:
+            x, y = map(float, line.split())
+            x_vals.append(x)
+            y_vals.append(y)
+        plt.plot(x_vals, y_vals)
+        plt.title(title)
+        plt.show()
 
 
 def main():
-    generate_input_file()
-    X, Y = read_data()
+    write_input_data()
+    X, Y = readdata()
     N = len(X) - 2
+    print(N)
+    R = 0
+    x = X[0]
+    h = (X[N] - X[0]) / (20 * N)
 
-    with open("outNn.txt", "wt") as file1, open("outWk.txt", "wt") as file2, open("outR.txt", "wt") as file3:
-        h = (X[N] - X[0]) / (20 * N)
-        x = X[0]
-        for _ in range(20*N+1):
-            file1.write(f"{x:e} {Nn(x, N, X, Y):e}\n")
+    with open("outNn.txt", "w") as file1, open("outWk.txt", "w") as file2, open("outR.txt", "w") as file3:
+        for _ in range(20 * N + 1):
+            file1.write(f"{x} {Nn(x, N, X, Y)}\n")
             R = abs(f(x) - Nn(x, N, X, Y))
-            file2.write(f"{x:e} {wkx(N, x, X):e}\n")
-            file3.write(f"{x:e} {R:e}\n")
+            file2.write(f"{x} {wkx(N, x, X)}\n")
+            file3.write(f"{x} {R}\n")
             x += h
+
+    plot_file("in.txt", "Input Data")
+    plot_file("outNn.txt", "Output Nn")
+    plot_file("outR.txt", "Output R")
+    plot_file("outWk.txt", "Output Wk")
 
 
 if __name__ == "__main__":
     main()
-    plot_file("outNn.txt", "Nn vs x", "x", "Nn(x)")
-    plot_file("outWk.txt", "wkx vs x", "x", "wkx(x)")
-    plot_file("outR.txt", "R vs x", "x", "R(x)")
