3e7fbc18bf18852f0f92e0f9fd8225b96890d2f8
Oleh <olegsuv.ukr@gmail.com>
2023-10-30 17:13:26 +0200

    add: lab-5 finished

--- fuzzy-systems/lab-5/main.py
+++ fuzzy-systems/lab-5/main.py
@@ -18,7 +18,6 @@ def transitive_closure(Q):
 # Знаходимо транзитивне замикання
 transitive_Q = transitive_closure(Q)
 
-# Виводимо транзитивне замикання
 print("Транзитивне замикання:")
 print(transitive_Q)
 
@@ -34,4 +33,31 @@ plt.figure(figsize=(8, 6))
 plt.imshow(transitive_Q, cmap='Blues', interpolation='none')
 plt.colorbar()
 plt.title("Транзитивне замикання")
+plt.show()
+
+
+people = ["Анна", "Борис", "Віктор", "Галина"]
+Q_other = np.array([[1, 0.6, 0.2, 0.8],
+                   [0.6, 1, 0.4, 0.7],
+                   [0.2, 0.4, 1, 0.5],
+                   [0.8, 0.7, 0.5, 1]])
+
+# Бінарне транзитивне замикання для людей
+transitive_Q_other = transitive_closure(Q_other)
+
+print("Транзитивне замикання для іншої сукупності:")
+print(transitive_Q_other)
+
+# Побудова графіку для бінарного нечіткого відношення Q_other
+plt.figure(figsize=(8, 6))
+plt.imshow(Q_other, cmap='Blues', interpolation='none')
+plt.colorbar()
+plt.title("Бінарне нечітке відношення Q_other")
+plt.show()
+
+# Побудова графіку для транзитивного замикання для іншої сукупності
+plt.figure(figsize=(8, 6))
+plt.imshow(transitive_Q_other, cmap='Blues', interpolation='none')
+plt.colorbar()
+plt.title("Транзитивне замикання для іншої сукупності")
 plt.show()
