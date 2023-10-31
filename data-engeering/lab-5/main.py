a92f87daca86decc5fdafda166ea05993cadb64a
Oleh <olegsuv.ukr@gmail.com>
2023-10-31 19:16:24 +0200

    add: options for country plot

--- data-engeering/lab-5/main.py
+++ data-engeering/lab-5/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 
-# Dataset was downloasded from:
+# Dataset was downloaded from:
 # https://www.kaggle.com/datasets/nelgiriyewithana/billionaires-statistics-dataset/
 df = pd.read_csv('BillionairesStatisticsDataset.csv')
 
@@ -17,13 +17,28 @@ human_worth_age = px.scatter(df, x='age', y='finalWorth',
 # Pie chart
 industry_counts = df['industries'].value_counts()
 industries_graph = px.pie(names=industry_counts.index, values=industry_counts.values,
-                   title='Галузі заробітку')
+                          title='Галузі заробітку')
 
 # Choropleth map for countryOfCitizenship
 humans_countries = df['countryOfCitizenship'].value_counts()
 maps_humans = px.choropleth(locations=humans_countries.index, locationmode="country names", color=humans_countries.values,
                             hover_name=humans_countries.index, color_continuous_scale="Viridis",
                             title='Кількість людей за громадянством')
+
+# Define the available options for choropleth map
+map_options = ['cpi_country', 'cpi_change_country', 'gdp_country',
+               'gross_tertiary_education_enrollment', 'gross_primary_education_enrollment_country',
+               'life_expectancy_country', 'total_tax_rate_country_country']
+
+# Create an initial choropleth map (using 'cpi_country' as default)
+initial_variable = 'cpi_country'
+map_figure = px.choropleth(locations=df['countryOfCitizenship'],
+                           locationmode="country names",
+                           color=df[initial_variable],
+                           hover_name=df['countryOfCitizenship'],
+                           color_continuous_scale="Viridis",
+                           title='Графік країн по характеристиці: ')
+
 
 app.layout = html.Div([
     html.Div([
@@ -35,9 +50,32 @@ app.layout = html.Div([
     ], style={'width': '49%', 'display': 'inline-block'}),
 
     html.Div([
-        dcc.Graph(figure=maps_humans)
-    ])
+        dcc.Graph(figure=maps_humans),
+    ], style={'width': '49%', 'display': 'inline-block'}),
+
+    html.Div([
+        dcc.Graph(id='choropleth-map', figure=map_figure),
+        dcc.Dropdown(id='variable-dropdown',
+                     options=[{'label': var, 'value': var}
+                              for var in map_options],
+                     value=initial_variable)
+    ], style={'width': '49%', 'display': 'inline-block'}),
 ])
+
+
+@app.callback(
+    Output('choropleth-map', 'figure'),
+    [Input('variable-dropdown', 'value')]
+)
+def update_choropleth_map(selected_variable):
+    updated_figure = px.choropleth(locations=df['countryOfCitizenship'],
+                                   locationmode="country names",
+                                   color=df[selected_variable],
+                                   hover_name=df['countryOfCitizenship'],
+                                   color_continuous_scale="Viridis",
+                                   title=f'Choropleth Map of {selected_variable}')
+
+    return updated_figure
 
 
 if __name__ == "__main__":
---
+++ data-engeering/lab-5/tasks.md
@@ -0,0 +1,4 @@
+# Graphs
+
+1. Slider for map plot of human worth (from min to max)
+2.
