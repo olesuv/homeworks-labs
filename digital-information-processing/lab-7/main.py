b2ee41c4c0e05086eaf955355a539c85fd0e8d46
Oleh <olegsuv.ukr@gmail.com>
2023-11-24 10:47:39 +0200

    mod: lab-7

--- digital-information-processing/lab-7/main.py
+++ digital-information-processing/lab-7/main.py
@@ -1,36 +1,76 @@
-import time
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq
+import time
 
-# Задання параметрів
-t = np.linspace(0, 480, 1000)  # Генерація послідовності t
-X_t = np.cos(2 * np.pi * t / 10 + 1) + np.cos(2 * np.pi * t / 40 + np.pi / 2)  # Обчислення X(t)
+# Функція для обчислення ШПФ
+def calculate_dft(x):
+    N = len(x)
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    e = np.exp(-2j * np.pi * k * n / N)
+    X = np.dot(e, x)
+    return X
 
-# ШПФ за допомогою scipy.fft
-X_f = fft(X_t)  # Виконання прямого ШПФ
-frequencies_fft = fftfreq(len(t), t[1] - t[0])
-periodogram_fft = np.abs(X_f) ** 2
+# Функція для обчислення зворотного ШПФ
+def calculate_idft(X):
+    N = len(X)
+    k = np.arange(N)
+    n = k.reshape((N, 1))
+    e = np.exp(2j * np.pi * k * n / N)
+    x = np.dot(e, X) / N
+    return x
 
-# Зворотне ШПФ
-X_t_reconstructed = np.fft.ifft(X_f)  # Виконання зворотного ШПФ
+# Задання формули 12
+def formula_12(t):
+    return np.cos(2 * np.pi * t / 10 + 1) + np.cos(2 * np.pi * t / 40 + np.pi / 2)
 
-# Час виконання ШПФ
+# Створення модельного ряду
+t_values = np.arange(0.7, 480, 0.1)
+x_values = formula_12(t_values)
+
+# Створення плоту для графіків
+fig, axes = plt.subplots(4, 1, figsize=(8, 10))
+
+# Виведення графіка модельного ряду
+axes[0].plot(t_values, x_values)
+axes[0].set_title('Модельний ряд (Формула 12)')
+axes[0].set_xlabel('Час')
+axes[0].set_ylabel('Значення')
+
+# Обчислення прямого ШПФ та виведення графіка амплітуд
 start_time = time.time()
-X_f = fft(X_t)
+X = calculate_dft(x_values)
 end_time = time.time()
-execution_time = end_time - start_time
-print(f"Час виконання ШПФ: {execution_time} секунд")
+execution_time_forward = end_time - start_time
 
+axes[1].plot(np.abs(X))
+axes[1].set_title('Амплітуди ШПФ')
+axes[1].set_xlabel('Частота')
+axes[1].set_ylabel('Амплітуда')
 
-# Відображення графіків
-plt.figure(figsize=(12, 6))
-plt.subplot(2, 1, 1)
-plt.plot(t, X_t)
-plt.title('Сигнал X(t)')
-plt.subplot(2, 1, 2)
-plt.plot(frequencies_fft, periodogram_fft)
-plt.title('Періодограма (ШПФ)')
-plt.xlabel('Частота')
-plt.ylabel('Потужність')
+# Обчислення зворотного ШПФ та виведення графіка відновленого ряду
+start_time = time.time()
+x_reconstructed = calculate_idft(X)
+end_time = time.time()
+execution_time_inverse = end_time - start_time
+
+axes[2].plot(t_values, x_reconstructed.real, label='Відновлений ряд')
+axes[2].plot(t_values, x_values, label='Оригінальний ряд', linestyle='--')
+axes[2].legend()
+axes[2].set_title('Відновлений ряд за допомогою зворотного ШПФ')
+axes[2].set_xlabel('Час')
+axes[2].set_ylabel('Значення')
+
+# Виведення часу виконання прямого та зворотного ШПФ
+print(f"Час виконання прямого ШПФ: {execution_time_forward} секунд")
+print(f"Час виконання зворотного ШПФ: {execution_time_inverse} секунд")
+
+# Виведення графіка фазової характеристики ШПФ
+axes[3].plot(np.angle(X))
+axes[3].set_title('Фазова характеристика ШПФ')
+axes[3].set_xlabel('Частота')
+axes[3].set_ylabel('Фаза')
+
+# Відображення плоту
+plt.tight_layout()
 plt.show()
