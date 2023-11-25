1074ea9bb6021a281d65056ce886605f0e0aee5d
Oleh <olegsuv.ukr@gmail.com>
2023-11-25 19:56:32 +0200

    mod: some works from local machine

--- digital-information-processing/lab-12/main.py
+++ digital-information-processing/lab-12/main.py
@@ -1,37 +1,27 @@
 import cv2
-from matplotlib import pyplot as plt
 
-# Зчитуємо зображення
-logo = cv2.imread("marker.jpg")
-img = cv2.imread("background.jpg")
+jpg_image = cv2.imread('background.jpg')
+png_image = cv2.imread('marker.png', cv2.IMREAD_UNCHANGED)
 
-# Отримуємо розміри зображень
-(wH, wW) = logo.shape[:2]
-(h, w) = img.shape[:2]
+# Отримуємо розміри основного та наложеного зображень
+height, width, _ = jpg_image.shape
+overlay_height, overlay_width, _ = png_image.shape
 
-# Обчислюємо координати для накладення маркера
-startY = h - wH - 10
-endY = h - 10
-startX = w - wW - 10
-endX = w - 10
+# Обчислюємо позицію для наложення
+x_position = int((width - overlay_width) / 2)
+y_position = int((height - overlay_height) / 2)
 
-# Вирізаємо область для маркера
-destination = img[startY:endY, startX:endX]
+# Налаштовуємо розмір та позицію наложеного зображення
+roi = jpg_image[y_position:y_position + overlay_height, x_position:x_position + overlay_width]
 
-# Змінюємо розміри зображення logo
-logo_resized = cv2.resize(logo, (destination.shape[1], destination.shape[0]))
+# Створюємо маску для альфа-каналу
+alpha_channel = png_image[:, :, 3] / 255.0
 
-# Застосовуємо змінення за допомогою addWeighted
-result = cv2.addWeighted(destination, 1, logo_resized, 0.7, 0)
-
-# Замінюємо вихідну область зображення результатом
-img[startY:endY, startX:endX] = result
+# Налаштовуємо наложення
+for c in range(0, 3):
+    jpg_image[y_position:y_position + overlay_height, x_position:x_position + overlay_width, c] = \
+        (1 - alpha_channel) * jpg_image[y_position:y_position + overlay_height, x_position:x_position + overlay_width, c] + \
+        alpha_channel * png_image[:, :, c]
 
 # Зберігаємо результат
-cv2.imwrite("watermarked.jpg", img)
-
-# Показуємо зображення за допомогою matplotlib
-RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(RGB_img)
-plt.show()
-
+cv2.imwrite('output_image.jpg', jpg_image)
