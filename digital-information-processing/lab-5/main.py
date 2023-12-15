65941c57527c525aba6a71efb92703bf81af8309
plxgwalker <olegsuv.ukr@gmail.com>
2023-12-15 11:25:42 +0200

    mod: lab-5

--- digital-information-processing/lab-5/main.py
+++ digital-information-processing/lab-5/main.py
@@ -1,37 +1,28 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from random import randint
 
-# Генеруємо випадковий сигнал
-rand_signals = []
-for n in range(100):
-    rand_signals.append(randint(1, 10))
 
-# Функція для обчислення кореляції
-def correlation(signal1, signal2):
-    mean1 = sum(signal1) / len(signal1)
-    mean2 = sum(signal2) / len(signal2)
-    numerator = sum((x - mean1) * (y - mean2) for x, y in zip(signal1, signal2))
-    denominator = (sum((x - mean1)**2 for x in signal1) * sum((y - mean2)**2 for y in signal2))**0.5
-    
-    if denominator != 0:
-        return numerator / denominator
-    else:
-        return 0.0  # Якщо denominator дорівнює нулю, повертаємо 0.0
+x = np.array([4, 2, -1, 3, -2, -6, -5, 4, 5])
+y = np.array([-4, 1, 3, 7, 4, -2, -8, -2, 1])
 
-# Обчислюємо кореляцію між двома випадковими сигналами
-correlation_result = correlation(rand_signals, rand_signals)
+correlation = np.correlate(x, y, mode='full')
+print("Кореляція двох сигналів:", correlation)
 
-# Виводимо результат кореляції
-print(f"Кореляція: {correlation_result}")
 
-# Функція для обчислення автокореляції
-def autocorrelation(signal):
-    return [correlation(signal[:-k], signal[k:]) for k in range(1, len(signal))]
+signal_length = 1000
+time = np.linspace(0, 1, signal_length, endpoint=False)
+frequency = 5
+amplitude = 1
+signal = amplitude * np.sin(2 * np.pi * frequency * time)
 
-# Обчислюємо автокореляцію
-autocorrelation_result = autocorrelation(rand_signals)
+autocorrelation = np.correlate(signal, signal, mode='full')
+autocorrelation = autocorrelation[signal_length-1:]
+autocorrelation /= np.max(autocorrelation)
 
-# Виводимо результат автокореляції
-plt.plot(autocorrelation_result)
-plt.title("Графік автокореляції")
+plt.figure()
+plt.plot(autocorrelation)
+plt.title('Автокореляція випадкового сигналу')
+plt.xlabel('Зсув часу')
+plt.ylabel('Значення автокореляції')
+plt.grid()
 plt.show()
