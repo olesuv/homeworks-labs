dbe18e9719b7102105d6704d3e9f46c1f9cb5665
Oleh <olegsuv.ukr@gmail.com>
2024-03-12 08:48:52 +0200

    mod: lab-4

--- software-testing/lab-4.py
+++ software-testing/lab-4.py
@@ -1,11 +1,15 @@
 import math
 
-
 def func(a, y):
+    try:
+       a, y = int(a), int(y)
+    except ValueError:
+        return "Parameters should be `int` type"
+
     try:
         res = math.sqrt(y**2 - 2*a)
     except ValueError:
-        return "Under sqrt should be bigger than 0"
+        return "Value under sqrt should be bigger than `0`"
 
     return a * res    
 
@@ -21,10 +25,11 @@ if __name__ == "__main__":
 
         for option_name, value in parameters.items():
             u_input = input(f"Enter {option_name}: ")
+
             if u_input == "exit":
                 exit()
 
-            parameters[option_name] = int(u_input)
+            parameters[option_name] = u_input
 
         res = func(parameters["a"], parameters["y"])
         print(f"Result: {res}\n")
